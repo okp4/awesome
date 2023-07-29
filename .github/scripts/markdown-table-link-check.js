f570cdc841c33ec4175dba6fb344c5e8c0e4c1d4
ccamel <camel.christophe@gmail.com>
2023-07-29 18:07:21 +0200

    ci(workflow): re-introduce link-check config

--- .github/scripts/markdown-table-link-check.js
+++ .github/scripts/markdown-table-link-check.js
@@ -1,29 +1,34 @@
-const fs = require('fs');
-const linkCheck = require('link-check');
-const LinkCheckResult = require('link-check').LinkCheckResult;
-const readmeContent = fs.readFileSync('./README.md', 'utf8');
-const { parseMarkdownTables } = require('./helper/index.js');
-const opt = require('../../.mlc_config.json');
-const { resolve } = require('dns');
+const fs = require("fs");
+const linkCheck = require("link-check");
+const LinkCheckResult = require("link-check").LinkCheckResult;
+const readmeContent = fs.readFileSync("./README.md", "utf8");
+const { parseMarkdownTables } = require("./helper/index.js");
+const opt = require("./.link-check.json");
+const { resolve } = require("dns");
 
-const replacementSymbolDead = '❌';
-const replacementSymbolAlive = '✅';
+const replacementSymbolDead = "❌";
+const replacementSymbolAlive = "✅";
 const parsedTable = parseMarkdownTables(readmeContent);
-const allLinksFromUrlsClm = parsedTable.map(table => table.url).flat().filter(Boolean);
+const allLinksFromUrlsClm = parsedTable
+    .map((table) => table.url)
+    .flat()
+    .filter(Boolean);
 
 async function linksCheck(links) {
     const linkCheckResults = [];
     for (const link of links) {
-        const res = await new Promise((resolve) => linkCheck(link, opt, (err, result) => {
-            if(err) {
-                console.warn(err)
-                return resolve({
-                    link,
-                    status: 'dead'
-                })
-            }
-            return resolve(result);
-        }));
+        const res = await new Promise((resolve) =>
+            linkCheck(link, opt, (err, result) => {
+                if (err) {
+                    console.warn(err);
+                    return resolve({
+                        link,
+                        status: "dead",
+                    });
+                }
+                return resolve(result);
+            })
+        );
         linkCheckResults.push(res);
     }
     return linkCheckResults;
@@ -31,27 +36,36 @@ async function linksCheck(links) {
 
 async function checkLinksInTables() {
     const results = await linksCheck(allLinksFromUrlsClm);
-    return results.reduce((acc, cur) => {
-        if(cur.status === 'dead') {
-            acc.arrDead.push(cur.link);
-        }
-        if(cur.status === 'alive'){
-            acc.arrAlive.push(cur.link)
-        }
-        return acc;
-    }, {arrDead: [], arrAlive: []})
+    return results.reduce(
+        (acc, cur) => {
+            if (cur.status === "dead") {
+                acc.arrDead.push(cur.link);
+            }
+            if (cur.status === "alive") {
+                acc.arrAlive.push(cur.link);
+            }
+            return acc;
+        },
+        { arrDead: [], arrAlive: [] }
+    );
 }
 
 async function updateLinks(arrDead, arrAlive, lines) {
     const updatedLines = lines.map((line) => {
         arrDead.forEach((link) => {
             if (line.includes(link)) {
-                line = line.replace(replacementSymbolAlive, replacementSymbolDead);
+                line = line.replace(
+                    replacementSymbolAlive,
+                    replacementSymbolDead
+                );
             }
         });
         arrAlive.forEach((link) => {
             if (line.includes(link)) {
-                line = line.replace(replacementSymbolDead, replacementSymbolAlive);
+                line = line.replace(
+                    replacementSymbolDead,
+                    replacementSymbolAlive
+                );
             }
         });
         return line;
@@ -59,11 +73,11 @@ async function updateLinks(arrDead, arrAlive, lines) {
     return updatedLines;
 }
 
-(async function(){
+(async function () {
     const { arrAlive, arrDead } = await checkLinksInTables();
-    const lines = readmeContent.split('\n');
+    const lines = readmeContent.split("\n");
 
     const updatedLines = await updateLinks(arrDead, arrAlive, lines);
-    const updatedTable = updatedLines.join('\n');
-    fs.writeFileSync('./README.md', updatedTable, 'utf8');
-})()
+    const updatedTable = updatedLines.join("\n");
+    fs.writeFileSync("./README.md", updatedTable, "utf8");
+})();
